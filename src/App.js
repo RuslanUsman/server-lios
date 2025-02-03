91d478b5cdeb5705769772ae57dda627209fcfce
Руслан Усманов <ruslanusmanov029@gmail.com>
2025-02-03 19:14:40 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -6,10 +6,16 @@ function App() {
       <h1>My Video Server</h1>
       <video width="320" height="240" controls>
         <source src="/videos/кинотеатр.mp4" type="video/mp4" />
-        <source src="/videos/кинотеатр.webm" type="video/webm" />
+        Your browser does not support the video tag.
+      </video>
+
+      <video width="320" height="240" controls>
         <source src="/videos/погода.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
+
+
+
     </div>
   );
 }
