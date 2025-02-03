d558e65a9e2140d3a6b5dc7d43fb5df9bcd6a838
Руслан Усманов <ruslanusmanov029@gmail.com>
2025-02-03 18:54:25 +0300

    Added video files

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ function App() {
   return (
     <div className="App">
       <h1>My Video Server</h1>
+      <video width="320" height="240" controls>
+        <source src="/videos/кинотеатр.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
     </div>
   );
 }
 
 export default App;
+
 
