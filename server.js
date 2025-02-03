a57b84f692c2046cbe7c8dbe7f79fff4a9ab6dff
Руслан Усманов <ruslanusmanov029@gmail.com>
2025-02-03 18:46:37 +0300

    Added video files

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Обслуживание статических файлов из папки 'build'
 app.use(express.static(path.join(__dirname, 'build')));
+
+// Обслуживание видео файлов из папки 'videos'
+app.use('/videos', express.static(path.join(__dirname, 'videos')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
